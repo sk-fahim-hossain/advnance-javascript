ae476a3de1d1f2f12ad5bb4a2c462944778d9dfd
sk-fahim-hossain <fahimhossainjob1@gmail.com>
2020-10-29 03:49:19 +0600

    array slice ,splice and join cleared

---
+++ array-slice.js
@@ -0,0 +1,9 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+const part = nums.slice(2, 6);
+console.log(part)
+
+const remove = nums.slice(2, 4);
+console.log(remove);
+
+const together = nums.join("+");
+console.log(together);
